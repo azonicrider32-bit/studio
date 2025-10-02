8bf99e38324b6a58696e234615ea87773ec204b8
brad bome <azonicrider32@gmail.com>
2025-10-02 02:59:36 +0000

    please tel me what is going on..is this cache issue???? becasue the pane

--- src/components/pro-segment-ai.tsx
+++ src/components/pro-segment-ai.tsx
@@ -546,8 +546,8 @@ export function ProSegmentAI() {
                     </TooltipProvider>
                   </SidebarHeader>
                   <Separator />
-                  <div className="flex-1 overflow-y-auto flex flex-col">
-                    <TabsContent value="tools" className={cn("m-0", activeTool === 'lasso' ? "flex-1 flex flex-col" : "")}>
+                  <div className="flex-1 overflow-y-auto flex flex-col min-h-0">
+                    <TabsContent value="tools" className={cn("m-0", activeTool === 'lasso' ? 'flex-1 flex flex-col' : '')}>
                         {renderRightPanelContent()}
                     </TabsContent>
                     <TabsContent value="feather" className="m-0">
@@ -598,5 +598,3 @@ export function ProSegmentAI() {
     </SidebarProvider>
   )
 }
-
-    
