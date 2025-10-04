7c3e996cb842a82ec7d680bb724a396040e33b1c
brad bome <azonicrider32@gmail.com>
2025-10-04 19:22:46 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase Storage:

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,10 @@
+
 export const firebaseConfig = {
   "projectId": "lumin-85363577-fc376",
   "appId": "1:698039461177:web:0e2463b9802e379b4bb4e8",
   "apiKey": "AIzaSyAzpNO1GQuVFKfuWUU5KxjpAYo8u1hwEyo",
   "authDomain": "lumin-85363577-fc376.firebaseapp.com",
+  "storageBucket": "lumin-853g3577-fc376.appspot.com",
   "measurementId": "",
   "messagingSenderId": "698039461177"
 };
