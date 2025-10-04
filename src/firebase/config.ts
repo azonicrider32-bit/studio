7762af1ef077146356dd73131ac9b8f1b15dd593
brad bome <azonicrider32@gmail.com>
2025-10-04 19:14:46 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "lumin-85363577-fc376",
+  "appId": "1:698039461177:web:0e2463b9802e379b4bb4e8",
+  "apiKey": "AIzaSyAzpNO1GQuVFKfuWUU5KxjpAYo8u1hwEyo",
+  "authDomain": "lumin-85363577-fc376.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "698039461177"
+};
